3e1be4a3500f0251371466a0f91d500942fab230
evandrolopes85 <evandrocwd@gmail.com>
2021-11-17 00:02:02 -0300

    Criando projeto

---
+++ src/main/java/com/github/evandrolopes85/temspotify/TemspotifyApplication.java
@@ -0,0 +1,13 @@
+package com.github.evandrolopes85.temspotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemspotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemspotifyApplication.class, args);
+	}
+
+}

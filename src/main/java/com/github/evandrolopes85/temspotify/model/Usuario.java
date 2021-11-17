8aa3de0bc30d639e1673c58e66dd71f570d462ea
evandrolopes85 <evandrocwd@gmail.com>
2021-11-17 00:33:43 -0300

    Criando e Anotando classe Usuario

---
+++ src/main/java/com/github/evandrolopes85/temspotify/model/Usuario.java
@@ -0,0 +1,59 @@
+package com.github.evandrolopes85.temspotify.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tbl_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_usuario")
+	private int idUsuario;
+	
+	@Column(name="nome", length=100)
+	private String nome;
+	
+	@Column(name="email", length=100)
+	private String email;
+	
+	@Column(name="senha", length=20)
+	private String senha;
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+}
